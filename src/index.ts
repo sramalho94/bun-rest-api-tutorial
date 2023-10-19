f7ce2872bb556b23bb611e58a9ab3eab16509f19
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-19 12:14:07 -0400

    Replace body with context

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello, I am practicing Bun and Elysia")
 .get('/post/:id', ({params: {id}}) => {return {id: id, title: 'Learn Bun'}})
-.post('/post', (body) => {return body})
+.post('/post', (context) => {return context})
 .get('/track/*', () => {return 'Track Route'})
 .listen(3000);
 

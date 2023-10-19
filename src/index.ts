c9cedd4fa46faac1ac4c056b31f6102da7c46481
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-19 12:13:11 -0400

    Add track route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Elysia } from "elysia";
 const app = new Elysia().get("/", () => "Hello, I am practicing Bun and Elysia")
 .get('/post/:id', ({params: {id}}) => {return {id: id, title: 'Learn Bun'}})
 .post('/post', (body) => {return body})
+.get('/track/*', () => {return 'Track Route'})
 .listen(3000);
 
 console.log(
